e1e3f286198218d98e2270e4330a6749026a9b4b
onlyet <hello@lol.com>
2022-07-19 02:02:14 +0800

    mux

--- NanaRecorder/mux.cpp
+++ NanaRecorder/mux.cpp
@@ -44,38 +44,79 @@ int Mux::writeHeader()
     return 0;
 }
 
-int Mux::writePacket()
+int Mux::writePacket(AVPacket* packet)
 {
+    int stream_index = packet->stream_index;
+    //printf("index:%d, pts:%lld\n", stream_index, packet->pts);
+
+    if (!packet || packet->size <= 0 || !packet->data) {
+        qDebug() << "packet is null";
+        if (packet) av_packet_free(&packet);
+
+        return -1;
+    }
+
+    AVRational src_time_base;   // 编码后的包
+    AVRational dst_time_base;   // mp4输出文件对应流的time_base
+    if (m_vStream && m_vEncodeCtx && stream_index == m_vIndex) {
+        src_time_base = m_vEncodeCtx->time_base;
+        dst_time_base = m_vStream->time_base;
+    }
+    else if (m_aStream && m_aEncodeCtx && stream_index == m_aIndex) {
+        src_time_base = m_aEncodeCtx->time_base;
+        dst_time_base = m_aStream->time_base;
+    }
+    // 时间基转换
+    packet->pts = av_rescale_q(packet->pts, src_time_base, dst_time_base);
+    packet->dts = av_rescale_q(packet->dts, src_time_base, dst_time_base);
+    packet->duration = av_rescale_q(packet->duration, src_time_base, dst_time_base);
+
+    int ret = av_interleaved_write_frame(m_oFmtCtx, packet);
+    //av_free_packet(&packet);
+    av_packet_free(&packet);
+    if (ret != 0) {
+		qDebug() << "video av_interleaved_write_frame failed, ret:" << ret;
+        return -1;
+    }
     return 0;
 }
 
 int Mux::writeTrailer()
 {
+    int ret = av_write_trailer(m_oFmtCtx);
     return 0;
 }
 
-int Mux::addStream()
+int Mux::addStream(AVCodecContext* encodeCtx)
 {
-    if (m_vFmtCtx->streams[m_vIndex]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO)
-    {
-        AVStream* vStream = nullptr, * aStream = nullptr;
-        vStream = avformat_new_stream(m_oFmtCtx, nullptr);
-        if (!vStream)
-        {
-            printf("can not new stream for output!\n");
-            return -1;
-        }
-        //AVFormatContext第一个创建的流索引是0，第二个创建的流索引是1
-        m_vOutIndex = vStream->index;
-        vStream->time_base = AVRational{ 1, m_fps };
+    AVStream* stream = avformat_new_stream(m_oFmtCtx, nullptr);
+	if (!stream)
+	{
+        qDebug() << "avformat_new_stream failed";
+		return -1;
+	}
+	//AVFormatContext第一个创建的流索引是0，第二个创建的流索引是1
+	//m_vOutIndex = vStream->index;
+	//vStream->time_base = AVRational{ 1, m_fps };
 
-        //将codecCtx中的参数传给输出流
-        ret = avcodec_parameters_from_context(vStream->codecpar, m_vEncodeCtx);
-        if (ret < 0)
-        {
-            qDebug() << "Output avcodec_parameters_from_context,error code:" << ret;
-            return -1;
-        }
+	//将codecCtx中的参数传给输出流
+	int ret = avcodec_parameters_from_context(stream->codecpar, encodeCtx);
+	if (ret < 0)
+	{
+		qDebug() << "Output avcodec_parameters_from_context,error code:" << ret;
+		return -1;
+	}
+
+
+    if (encodeCtx->codec_type == AVMEDIA_TYPE_VIDEO) {
+        m_vEncodeCtx = encodeCtx;
+        m_vStream = stream;
+        m_vIndex = stream->index;
+    }
+    else if (encodeCtx->codec_type == AVMEDIA_TYPE_AUDIO) {
+        m_aEncodeCtx = encodeCtx;
+        m_aStream = stream;
+        m_aIndex = stream->index;
     }
 
     return 0;
--- NanaRecorder/mux.h
+++ NanaRecorder/mux.h
@@ -11,13 +11,21 @@ public:
     int init(const std::string& filename);
     void deinit();
     int writeHeader();
-    int writePacket();
+    int writePacket(AVPacket* packet);
     int writeTrailer();
 
-    int addStream();
+    int addStream(AVCodecContext* encodeCtx);
 
 private:
     AVFormatContext*    m_oFmtCtx = nullptr;
     std::string         m_filename;
+
+    AVCodecContext*     m_vEncodeCtx = nullptr;
+    AVCodecContext*     m_aEncodeCtx = nullptr;
+    AVStream*           m_vStream = nullptr;
+    AVStream*           m_aStream = nullptr;
+    int                 m_vIndex;
+    int                 m_aIndex;
+
 };
 
